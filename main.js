99014399081963f91a986f3447dcdbe2660e1694
Kay Mok <kaymok3@gmail.com>
2018-03-13 11:43:37 -0400

    Refactor further

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+const sectionOne = document.getElementById('one');
+const sectionTwo = document.getElementById('two');
+const sectionThree = document.getElementById('three');
+const sectionFour = document.getElementById('four');
+
 function scroll(pixels, section) {
   if (document.documentElement.scrollTop > pixels) {
     section.className = 'show';
@@ -6,26 +11,9 @@ function scroll(pixels, section) {
   }
 }
 
-const sectionOne = document.getElementById('one');
-
 window.addEventListener('scroll', function() {
     scroll(0, sectionOne);
-});
-
-const sectionTwo = document.getElementById('two');
-
-window.addEventListener('scroll', function() {
     scroll(800, sectionTwo);
-});
-
-const sectionThree = document.getElementById('three');
-
-window.addEventListener('scroll', function() {
     scroll(1600, sectionThree);
-});
-
-const sectionFour = document.getElementById('four');
-
-window.addEventListener('scroll', function() {
-  scroll(2400, sectionFour);
+    scroll(2400, sectionFour);
 });
